28404e45a4ab2094ee96c0e4146f8e70be832506
IrinaArmstrong <a.irene.a@mail.ru>
2023-04-13 18:39:40 +0300

    started benchmark

---
+++ benchmark/tasks/bigbench_task.py
@@ -0,0 +1,52 @@
+import datasets
+from typing import (Dict, Any, Optional)
+
+
+from romashka.logging_handler import get_logger
+from romashka.benchmark.tasks.text_task_abstract import AbstractTextTask
+
+class BigBenchTaskDataset(AbstractTextTask):
+
+    def __init__(self):
+        self.task_name = None
+
+    def __post_init__(self):
+        self.split_to_data_split = {"train": "train"}
+        self.name = "" if self.name is None else self.name
+        self.data_path = self.data_path if self.data_path is not None \
+            else f"{DATA_PATHS_MAPPING['Flan_raw']}bigbench_{self.name}/"
+        self.logger = get_logger(
+            name=self.__class__.__name__,
+            logging_level="DEBUG" if self.verbose else "INFO"
+        )
+
+    def load_dataset(self, extension: Optional[str] = "json",
+                     data_files: Optional[Dict[str, str]] = None,
+                     split: str = None, **kwargs):
+        try:
+            return super().load_dataset_local(path=self.data_path,
+                                              data_files=data_files,
+                                              split=split
+                                              )
+        except Exception as e:
+            self.logger.error(f"Error during AbstractTaskDataset.load_dataset() method occurred: {e}. "
+                              f"Loading dataset with datasets.load_from_disk() method...")
+            return datasets.load_from_disk(self.data_path)
+
+    def preprocessor(self, example: Dict[str, Any], add_prefix: bool = False, **kwargs):
+        prompts = json.load(open(PROMT_PATH))
+        prompt = random.choice(prompts[self.name])
+
+        if len(example['multiple_choice_targets']) > 0:
+            options = 'OPTIONS: - ' + '; - '.join(example['multiple_choice_targets'])
+        else:
+            options = ''
+        if self.task_name in ['conceptual_combinations', 'language_identification']:
+            options = ''
+        if 'russian' in self.name:
+            options = 'ВАРИАНТЫ: - ' + '; - '.join(example['multiple_choice_targets'])
+
+        src_texts = [prompt, example['inputs'], options] if options != "" else [prompt, example['inputs']]
+        tgt_texts = ['; '.join(example['targets'])]
+
+        return self.format_as_seq2seq(src_texts, tgt_texts)
---
+++ benchmark/tasks/text_autotask.py
@@ -0,0 +1,52 @@
+import json
+
+DATA_PATHS_MAPPING = {
+    "BigBench": "../data/bigbench/",
+    "MMLU": "./data/mmlu/",
+}
+
+PROMT_PATH = "../assets/prompts.json"
+ALL_TASKS_FN = "../assets/benchmark_tasks.json"
+ALL_TASKS = json.load(open(ALL_TASKS_FN))
+
+# DTO
+from collections import OrderedDict
+from typing import (Callable, Dict, Mapping, List,
+                    Any, Optional, Union)
+
+from romashka.logging_handler import get_logger
+from romashka.benchmark.tasks.bigbench_task import BigBenchTaskDataset
+
+"""
+AutoTask is created so that you can automatically retrieve the relevant dataset/task
+given the name and other arguments.
+"""
+
+logger = get_logger(
+    name="TextAutoTask",
+    logging_level="INFO"
+)
+
+BigBench_TASKS = ALL_TASKS['bigbench']
+BigBench_CLASSES = [BigBenchTaskDataset for _ in range(len(BigBench_TASKS))]
+BigBench_CLASSES = [(task_name, base_class) for (task_name, base_class) in zip(BigBench_TASKS, BigBench_CLASSES)]
+
+TEXT_TASK_LIST = BigBench_CLASSES  # add here other tasks
+TEXT_TASK_MAPPING = OrderedDict(TEXT_TASK_LIST)
+
+class AutoTextTask:
+
+    @classmethod
+    def get(cls, task_name: str, seed: Optional[int] = 42, **kwargs):
+        try:
+            return TEXT_TASK_MAPPING[task_name](seed=seed, **kwargs)
+        except Exception as e:
+            logger.error(f"Error during AutoTextTask creation with `task_name`-`{task_name}`\n:{e}")
+            raise ValueError(f"Error during AutoTextTask creation with `task_name`-`{task_name}`\n:{e}")
+
+
+def help_task_selection():
+    s = f"To create AutoTextTask select one from implemented tasks by it's name."
+    for task_name in TEXT_TASK_MAPPING:
+        s += "\n\t" + task_name
+    print(s)
