01cb864a5565b51dc6b681b0a0ed71ceccbffc14
IrinaArmstrong <a.irene.a@mail.ru>
2023-08-23 16:05:28 +0300

    added Intruct-Q-Former connector

--- transactions_qa/layers/__init__.py
+++ transactions_qa/layers/__init__.py
@@ -3,7 +3,10 @@ from romashka.transactions_qa.layers.connector import (CONNECTOR_TYPES,
                                                        make_complex_linear_connector,
                                                        make_recurrent_connector,
                                                        make_transformer_connector,
-                                                       make_qformer_connector)
+                                                       make_qformer_connector,
+                                                       make_instruct_qformer_connector)
+from romashka.transactions_qa.layers.qformer_connector_hf import QFormerConnector
+from romashka.transactions_qa.layers.instruct_qformer_connector_hf import InstructQFormerConnector
 
 __all__ = [
     "CONNECTOR_TYPES",
@@ -11,5 +14,8 @@ __all__ = [
     "make_complex_linear_connector",
     "make_recurrent_connector",
     "make_transformer_connector",
-    "make_qformer_connector"
+    "make_qformer_connector",
+    "make_instruct_qformer_connector",
+    "QFormerConnector",
+    "InstructQFormerConnector"
 ]
