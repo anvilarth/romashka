fixed qformer bug with data leaking through mask

transactions_qa/layers/qformer_connector_hf.py
@@ -116,7 +116,7 @@ class QFormerConnector(nn.Module):
         self.load_state_dict(checkpoint, strict=False)
         self._logger.info(f"Connector weights initialized from checkpoint: {ckpt_path}")
 
-    def forward(self, embeds: torch.Tensor,
+    def forward(self, embeds: torch.Tensor, mask: torch.Tensor,
                 output_attentions: Optional[bool] = False,
                 output_hidden_states: Optional[bool] = False,
                 return_dict: Optional[bool] = True,
@@ -124,13 +124,14 @@ class QFormerConnector(nn.Module):
 
         # step 1: get embeddings -> done!
         # step 2: forward the query tokens through the QFormer, using input embeddings for cross-attention
-        embeds_attention_mask = torch.ones(embeds.size()[:-1], dtype=torch.long, device=embeds.device)
+        # embeds_attention_mask = torch.ones(embeds.size()[:-1], dtype=torch.long, device=embeds.device)
+        
 
         query_tokens = self.query_tokens_embeddings.expand(embeds.shape[0], -1, -1)
         query_outputs = self.qformer(
             query_embeds=query_tokens,
             encoder_hidden_states=embeds,
-            encoder_attention_mask=embeds_attention_mask,
+            encoder_attention_mask=mask,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,