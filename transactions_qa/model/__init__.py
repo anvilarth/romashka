72d3041f4c51d5488e8204b545eb14756b766638
IrinaArmstrong <a.irene.a@mail.ru>
2023-11-24 15:12:22 +0300

    fixed import

--- transactions_qa/model/__init__.py
+++ transactions_qa/model/__init__.py
@@ -1,7 +1,7 @@
 from romashka.transactions_qa.model.text_model import TextEncoderModel
 from romashka.transactions_qa.model.pooler import PoolerType
 from romashka.transactions_qa.model.projection import ProjectionsType
-from romashka.transactions_qa.model.tqa_model import ContrastiveTransactionsModel
+from romashka.transactions_qa.model.trainer_model import ContrastiveTransactionsModel
 
 __all__ = [
     "TextEncoderModel",
